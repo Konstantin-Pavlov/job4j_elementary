77b4959ac8a2d13102fb8b2e43db1489aa1c9d86
Konstantin Pavlov <konstpavlov87@gmail.com>
2022-06-06 00:21:31 +0300

    4.2. display today's date

---
+++ src/main/java/ru/job4j/Info.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Info {
+    public static void main(String[] args) {
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy"); // HH:mm:ss
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+    }
+}
