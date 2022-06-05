7c637f043ba6bb04183691870f3b9cdb8251f523
Konstantin Pavlov <konstpavlov87@gmail.com>
2022-06-06 00:29:15 +0300

    4.2. исправлено нарушение - (Код содержит однострочные комментарии).

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class Info {
     public static void main(String[] args) {
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy"); // HH:mm:ss
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
         Date date = new Date();
         System.out.println(dateFormat.format(date));
     }
