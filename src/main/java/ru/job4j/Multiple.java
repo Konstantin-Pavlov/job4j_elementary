8e345598fadfa4e680cd84e274a07a07ffc12d51
Konstantin Pavlov <konstpavlov87@gmail.com>
2022-06-06 23:17:52 +0300

    4.1. Разбор первой программы. [#159033 #309077]. Убраны лишние переменные.

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,10 +2,8 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
-        final byte multiplier = 1;
-        final byte limit = 10;
-        for (int i = 1; i < limit; i++) {
-            System.out.printf("%d * %d = %d\n", multiplier, i, multiplier * i);
+        for (int i = 1; i < 10; i++) {
+            System.out.printf("%d * %d = %d\n", 1, i, 1 * i);
         }
     }
 }
