51934d9009fe610a3fa5b12fac03d1cb647cae4f
Konstantin Pavlov <konstpavlov87@gmail.com>
2022-06-07 14:22:55 +0300

    6. Checkstyle - fixed

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -4,5 +4,8 @@ public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Konstantin Pavlov");
         System.out.println();
+        int age = 18;
+        float length = 78.0332f;
+        boolean baby = true;
     }
 }
